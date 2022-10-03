39ff56abe80fa47d3ca98af3b4ed17bbffc3be84
ToniFeliu <91744505+ToniFeliudeC@users.noreply.github.com>
2022-10-03 16:24:05 +0200

    Initial commit

---
+++ contenidoVariables/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <?php
+
+        function isTrue($thing) {
+            if ($thing == 1) {
+                return "true";
+            }
+            else {
+                return "false";
+            }
+        }
+
+        $values = array(null, 0, true, false, "0", "", "foo", array(), null);
+        $valuesContent = array("null", 0, "true", "false", '"0"', '""', "foo", "array()", "unset(\$var)");
+
+        echo "<style>
+        th {
+            background-color: green;
+        }
+        table, th, td {
+            text-align: center;}</style>";
+        echo "<table style=width:100%>";
+        echo "<tr>
+        <th>Contenido de \$var</th>
+        <th>isset(\$var)</th>
+        <th>empty(\$var)</th>
+        <th>bool(\$var)</th>";
+        echo "</tr>";
+        for ($x = 0; $x < count($values); $x++) {
+            echo "<tr>";
+            echo "<td>" . $valuesContent[$x] . "</td>";
+            echo "<td>" . isTrue(isset($values[$x]))  . "</td>";
+            echo "<td>" . isTrue(empty($values[$x])) . "</td>";
+            echo "<td>" . isTrue((bool) $values[$x]) . "</td>"; 
+            echo "</tr>";
+        }
+        echo "</table>";
+
+    ?>
+</body>
+</html>
